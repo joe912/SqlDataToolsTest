1ce498feb657da5140d19e603456a91947eeb90d
joseph dillon <joseph.dillon@vercoglobal.com>
2017-09-29 09:13:35 +0100

    Add project files.

---
+++ SqlDataToolsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlDataToolsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
